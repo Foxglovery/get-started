3913c736cb396b7f6aff3a4337632244844a18d7
Gabriel Ferguson <131572832+Foxglovery@users.noreply.github.com>
2024-03-21 11:15:44 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
 import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
 
-const firebaseApp = initializeApp ({
-    apiKey: "AIzaSyBoTZEf7LmpDA3KOAok8ZPcPHkQDADRNU0",
-    authDomain: "fox-will-get.firebaseapp.com",
-    databaseURL: "https://fox-will-get-default-rtdb.firebaseio.com",
-    projectId: "fox-will-get",
-    storageBucket: "fox-will-get.appspot.com",
-    messagingSenderId: "45651837070",
-    appId: "1:45651837070:web:d13e8d9df67322f0f149df",
-    measurementId: "G-VSSVK52183"
-});
 const auth = getAuth(firebaseApp)
 
 
